5bea37572868fec755c8a510492f437232219cb0
jhjohnson777 <105759470+jhjohnson777@users.noreply.github.com>
2022-06-15 16:55:02 -0700

    Project1 Intro to JavaScript

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+window.alert("Hello, World!");  //  Window pops up with Hello, World!
+
+var Family = "The Johnsons", Dad = "Jack ", Mom = "Sarah", Son1 = "Soren", Son2 = "Henrik";   //defines multiple veriables of a family
+
+document.write(Son1)    //prints variable Son1 in browser window
+
+document.write(5 + 8)   //prints 13 in browser window
+
+document.write(Dad +"and " + Mom)  // prints Jack and Sarah
